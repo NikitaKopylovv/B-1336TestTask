88f08533148213f240354900a50ac3d63425b5ab
Kopylov Nikita <74319066+NikitaKopylovv@users.noreply.github.com>
2024-04-21 20:27:37 +0500

    corrected size of filters

--- src/Components/Brigade/BrigadeFilters.tsx
+++ src/Components/Brigade/BrigadeFilters.tsx
@@ -13,10 +13,10 @@ interface BrigadeFiltersProps {
 const BrigadeFilters: React.FC<BrigadeFiltersProps> = ({ connectionStates, departments, handleDepartmentChange, handleConnectionChange }) => {
     return (
         <Flex className='filter-flex-container' gap="middle">
-            <Select size="middle" className="filter-select-connection" placeholder="Соединение" allowClear onChange={handleConnectionChange}>
+            <Select size="large" className="filter-select-connection" placeholder="Соединение" allowClear onChange={handleConnectionChange}>
                 {connectionStates.map(state => (<Select.Option key={state.connectionStateId} value={state.connectionStateId}>{state.name}</Select.Option>))}
             </Select>
-            <Select placeholder="Департамент" className="filter-select-department" allowClear onChange={handleDepartmentChange}>
+            <Select size="large" placeholder="Департамент" className="filter-select-department" allowClear onChange={handleDepartmentChange}>
                 {departments.map(department => (<Select.Option key={department?.id} value={department.id}>{department.name}</Select.Option>))}
             </Select>
         </Flex>
